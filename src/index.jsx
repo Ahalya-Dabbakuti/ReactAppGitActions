094515398d44b036783f8d1c272bad1c3f9f4b91
Ahalya <dabbakutiahalya@gmail.com>
2025-09-05 12:13:15 +0530

    Added ReactApp-V1.0

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+export default function Index(){
+    return(
+        <div>
+            <h1>Welcome to Devops</h1>
+            <h1>Deploying the React App Automatically</h1>
+        </div>
+    );
+}
